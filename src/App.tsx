514f09c0f4e7ce0a84bf7a1d66399df573c74774
budocay <budocay972@gmail.com>
2021-03-20 18:17:24 +0100

    master: Display how much pokemon it exist in config array pokemon.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent, useState} from 'react';
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
   
 const App: FunctionComponent = () => {
- const name: String = 'React';
-    
+ const [pokemons, setpokemon] = useState<Pokemon[]>(POKEMONS);
+
  return (
-  <h1>Hello, {name} !</h1>
+  <h1>Il y a {pokemons.length} pokemon !</h1>
  )
 }
   
 export default App;
+
+//TODO:
+// Définir une variable d'état nommé pokemons dans le composant app.tsx
+// Initialiser l'état avec la liste de pokemon contenu dans la constante pokemons
+// Typer l'état pokemon pour qu'ils contiennent un array de pokémon
+// Afficher le nombre de pokemon présent dans l'état du composant
